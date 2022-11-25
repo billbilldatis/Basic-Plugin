7e114ac430a171dcd8adb5967030790e445222b9
Ntatis Vasileios <40182400+billbilldatis@users.noreply.github.com>
2022-11-26 00:13:03 +0200

    added files

---
+++ BasicPlugin/src/main/java/nl/codingtime/tutorialplugin/TutorialPlugin.java
@@ -0,0 +1,13 @@
+package nl.codingtime.tutorialplugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TutorialPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
---
+++ mc/src/main/java/package-info.java
@@ -0,0 +1,10 @@
+public class TutorialPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
